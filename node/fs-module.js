05cbbd5dbd45c4337aab74047f4c1440f51b1909
Andre Tavara <andretj_92@live.com>
2025-05-31 10:12:48 -0500

    Se agrega la clase del sabado - semana 13

---
+++ node/fs-module.js
@@ -0,0 +1,17 @@
+const newNote = { id: 7, content: "Nueva nota" };
+
+fs.readFile("./notes.json", "utf8", (err, content) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const notes = JSON.parse(content);
+  notes.push(newNote);
+
+  fs.writeFile("./notes.json", JSON.stringify(notes), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
--- node/server.js
+++ node/server.js
@@ -1,10 +1,5 @@
 import http from "node:http"; // EcmaScript6 Module
-
-const notes = [
-  { id: 1, content: "Hacer la cama" },
-  { id: 2, content: "Lavar los platos" },
-  { id: 3, content: "Estudiar JavaScript" },
-];
+import fs from "node:fs";
 
 function isUrlValid(url) {
   const urlArray = url.split("/"); // ['', 'notes', '3']
@@ -14,40 +9,28 @@ function isUrlValid(url) {
 
 function requestListener(request, response) {
   if (request.url === "/") {
-    const html = `<!DOCTYPE html>
-                  <html lang="en">
-                  <head>
-                      <meta charset="UTF-8">
-                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                      <title>Document</title>
-                  </head>
-                  <body>
-                      <h1>Bienvenido a Notas</h1>
-                  </body>
-                  </html>`;
+    fs.readFile("./pages/index.html", "utf8", (err, content) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
 
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(html);
-    response.end();
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+    });
   } else if (request.url === "/notes") {
-    const liElements = notes.map((note) => `<li>${note.content}</li>`).join("");
-    const html = `<!DOCTYPE html>
-                  <html lang="en">
-                    <head>
-                      <meta charset="UTF-8" />
-                      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                      <title>Document</title>
-                    </head>
-                    <body>
-                      <h1>Notas:</h1>
-                      <ol>
-                        ${liElements}
-                      </ol>
-                    </body>
-                  </html>`;
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(html);
-    response.end();
+    fs.readFile("./pages/notes.html", "utf8", (err, content) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      // const liElements = notes.map((note) => `<li>${note.content}</li>`).join("");
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+    });
   } else if (isUrlValid(request.url)) {
     const urlArray = request.url.split("/");
     urlArray.shift();
@@ -65,9 +48,16 @@ function requestListener(request, response) {
       response.end();
     }
   } else {
-    response.writeHead(404, { "Content-Type": "application/json" });
-    response.write(JSON.stringify({ error: "url no encontrada" }));
-    response.end();
+    fs.readFile("./pages/404.html", "utf8", (err, content) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      response.writeHead(404, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+    });
   }
 }
 
