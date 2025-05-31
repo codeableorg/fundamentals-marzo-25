5401aa2cad8d307ebefee02c9562b9ee545f7f65
Andre Tavara <andretj_92@live.com>
2025-05-30 21:07:17 -0500

    Se agrega la clase del viernes - semana 13

--- node/server.js
+++ node/server.js
@@ -1,31 +1,72 @@
-import http from "node:http";
+import http from "node:http"; // EcmaScript6 Module
 
 const notes = [
   { id: 1, content: "Hacer la cama" },
   { id: 2, content: "Lavar los platos" },
   { id: 3, content: "Estudiar JavaScript" },
-  { id: 4, content: "Hacer las compras" },
-  { id: 5, content: "Limpiar mi habitación" },
-  { id: 6, content: "Almorzar a tiempo" },
 ];
 
-function isUrlValid(url) {}
+function isUrlValid(url) {
+  const urlArray = url.split("/"); // ['', 'notes', '3']
+  urlArray.shift(); // ['notes', '3']
+  return urlArray.length === 2 && urlArray[0] === "notes";
+}
 
 function requestListener(request, response) {
   if (request.url === "/") {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Bienvenido a Notas");
+    const html = `<!DOCTYPE html>
+                  <html lang="en">
+                  <head>
+                      <meta charset="UTF-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <title>Document</title>
+                  </head>
+                  <body>
+                      <h1>Bienvenido a Notas</h1>
+                  </body>
+                  </html>`;
+
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(html);
     response.end();
   } else if (request.url === "/notes") {
-    const notesJson = JSON.stringify(notes);
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.write(notesJson);
+    const liElements = notes.map((note) => `<li>${note.content}</li>`).join("");
+    const html = `<!DOCTYPE html>
+                  <html lang="en">
+                    <head>
+                      <meta charset="UTF-8" />
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                      <title>Document</title>
+                    </head>
+                    <body>
+                      <h1>Notas:</h1>
+                      <ol>
+                        ${liElements}
+                      </ol>
+                    </body>
+                  </html>`;
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(html);
     response.end();
   } else if (isUrlValid(request.url)) {
-    const note = notes.find();
-    const noteJson = JSON.stringify(note);
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.write(noteJson);
+    const urlArray = request.url.split("/");
+    urlArray.shift();
+    const noteId = Number(urlArray[1]);
+    const note = notes.find((note) => note.id === noteId);
+
+    if (note) {
+      const noteJson = JSON.stringify(note);
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.write(noteJson);
+      response.end();
+    } else {
+      response.writeHead(404, { "Content-Type": "application/json" });
+      response.write(JSON.stringify({ error: "Nota no encontrada" }));
+      response.end();
+    }
+  } else {
+    response.writeHead(404, { "Content-Type": "application/json" });
+    response.write(JSON.stringify({ error: "url no encontrada" }));
     response.end();
   }
 }
